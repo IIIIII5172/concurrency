63e169d241eb00869d62b5957d33cece5763cff2
IIIIII5172 <957430010@qq.com>
2019-01-02 23:54:03 +0800

    add annoations

---
+++ src/main/java/com/mmall/concurrency/annoations/NotRecommend.java
@@ -0,0 +1,18 @@
+package com.mmall.concurrency.annoations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author IIIIII
+ * @create 2019-01-02-22:58
+ * @package com.mmall.concurrency.annoations
+ * 课程里用来标记【推荐写法】的注解
+ **/
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface Recommend {
+    String value() default "";
+}
---
+++ src/main/java/com/mmall/concurrency/annoations/ThreadSafe.java
@@ -0,0 +1,9 @@
+package com.mmall.concurrency.annoations;
+
+/**
+ * @author IIIIII
+ * @create 2019-01-02-22:58
+ * @package com.mmall.concurrency.annoations
+ **/
+public @interface ThreadSafe {
+}
